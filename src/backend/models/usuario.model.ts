7662f42432f9c1d6396b5c37f23cbd04618b51d8
fabiana.souza <fabiana.souza@sou.inteli.edu.br>
2026-05-25 10:01:55 -0300

    feat(#98): adiciona o usuario.models

---
+++ src/backend/models/usuario.model.ts
@@ -0,0 +1,22 @@
+export type UsuarioStatus = 'ativo' | 'inativo'
+export type UsuarioCargo = 'capataz' | 'supervisor' | 'gerente'
+
+export interface Usuario {
+  id: string
+  retiro_id: string
+  nome: string
+  login: string
+  senha_hash: string
+  status: UsuarioStatus
+  data_criacao: Date
+  cargo: UsuarioCargo
+}
+
+export interface UsuarioInput {
+  retiro_id: string
+  nome: string
+  login: string
+  senha_hash: string
+  status: UsuarioStatus
+  cargo: UsuarioCargo
+}
