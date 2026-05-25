606a4db22c8489d746d472208f7c1b6fa2ba4b5f
fabiana.souza <fabiana.souza@sou.inteli.edu.br>
2026-05-25 14:34:15 -0300

    feat(#98): adiciona evidencia-foto.model

---
+++ src/backend/models/evidencia-foto.model.ts
@@ -0,0 +1,12 @@
+export interface EvidenciaFoto {
+  evidencia_id: string
+  url_arquivo: string
+  latitude: number
+  longitude: number
+}
+
+export interface EvidenciaFotoInput {
+  url_arquivo: string
+  latitude: number
+  longitude: number
+}
