3bd2b2435093162ee0f7311fcaa933be1b851815
Levi Correia <levi.silveira@sou.inteli.edu.br>
2026-06-11 15:37:15 -0300

    feat(#195): Adicão de front end

--- capataz-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CapatazApp from "./CapatazApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CapatazApp />
-  </React.StrictMode>
-);
